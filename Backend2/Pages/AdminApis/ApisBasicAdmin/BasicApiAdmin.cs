4332dfe4da95363610a9faa491e0a62bd6afba71
Hossynkoala <Hossynassghary@gmail.com>
2020-10-06 00:38:58 +0330

    Apis admin add

---
+++ Backend2/Pages/AdminApis/ApisBasicAdmin/BasicApiAdmin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend2.Pages.AdminApis.ApisBasicAdmin
+{
+    public class BasicApiAdmin : ControllerBase
+    {
+        internal MongoClient Client = new MongoClient();
+        internal string AdminDatabase => "Users";
+        internal string AdminCollection => "Admins";
+    }
+}
---
+++ Backend2/Pages/AdminApis/PageAUT_Admin/PageAUT_Admin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend2.Pages.AdminApis.ApisBasicAdmin;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Backend2.Pages.AdminApis.PageAUT_Admin
+{
+    [Controller]
+    public class PageAUT_Admin : BasicApiAdmin
+    {
+        [HttpPost]
+        public async Task<bool> Login(string Username, string Password)
+        {
+            var Quary = new BsonDocument { { "Account.Username", Username }, { "Account.Password", Password } };
+
+
+            var Result = await Client.GetDatabase(AdminDatabase).GetCollection<BsonDocument>(AdminCollection).FindAsync(Quary).Result.SingleAsync();
+
+            if (Result.ElementCount >= 1)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
