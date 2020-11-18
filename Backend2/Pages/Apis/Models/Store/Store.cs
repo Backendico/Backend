ad8bc3b88c8cbd10873b7c8e5055c35a7ffab678
Hossynkoala <Hossynassghary@gmail.com>
2020-11-19 01:50:16 +0330

    add Page store

---
+++ Backend2/Pages/Apis/Models/Store/Store.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend2.Pages.Apis.Models.Store
+{
+    public class Store : BasicAPIs
+    {
+        internal async Task<bool> AddStore(string Token, string Studio, BsonDocument Detail)
+        {
+            if (await CheackToken(Token))
+            {
+                var Update = new UpdateDefinitionBuilder<BsonDocument>().Push("Store", Detail);
+
+                var Result = await Client.GetDatabase(Studio).GetCollection<BsonDocument>("Users").UpdateOneAsync(new BsonDocument { { "_id", "Setting" } }, Update);
+
+                if (Result.ModifiedCount >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
